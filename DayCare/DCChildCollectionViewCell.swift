3d6fece6c495e905fce26acc0c13f4a642c87c35
Rishabh Tayal <rtayal11@gmail.com>
2016-03-27 21:45:56 -0500

    added images

---
+++ DayCare/DCChildCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DCChildCollectionViewCell.swift
+//  DayCare
+//
+//  Created by Tayal, Rishabh on 3/27/16.
+//  Copyright © 2016 Tayal, Rishabh. All rights reserved.
+//
+
+import UIKit
+
+class DCChildCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
--- DayCare/DCViewController.swift
+++ DayCare/DCViewController.swift
@@ -8,38 +8,53 @@
 
 import UIKit
 import Parse
+import AFNetworking
 
 class DCViewController: UIViewController {
 
     @IBOutlet var collectionView: UICollectionView!
     
+    var dataArray: [PFObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PFCloud.callFunctionInBackground("hello", withParameters: nil) { (result: AnyObject?, error: NSError?) in
-            print(result)
+//        PFCloud.callFunctionInBackground("hello", withParameters: nil) { (result: AnyObject?, error: NSError?) in
+//            print(result)
+//        }
+
+        let query = PFQuery(className: "Child")
+        query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) in
+            self.dataArray = objects!
+            self.collectionView.reloadData()
         }
         
-        let obj = PFObject(className: "classNp")
-        obj["a"] = "b"
-        obj.saveInBackground()
+        collectionView.alwaysBounceVertical = true        
     }
 }
 
 extension DCViewController: UICollectionViewDataSource {
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return dataArray.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! DCChildCollectionViewCell
+        
+        let child = dataArray[indexPath.row]
         
         cell.backgroundColor = .grayColor()
+        if let url = child["image"] as? String {
+            cell.imageView.setImageWithURL(NSURL(string: url)!)
+        }
+        cell.nameLabel.text = child["name"] as? String
         
         return cell
     }
 }
 
 extension DCViewController: UICollectionViewDelegate {
-    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("selected " + String(indexPath.row))
+    }
 }
