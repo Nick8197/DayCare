16229fe0601086745605f898f0512053520b551b
Rishabh Tayal <rtayal11@gmail.com>
2016-04-04 10:42:23 -0500

    commit

--- DayCare/DCServiceCaller.swift
+++ DayCare/DCServiceCaller.swift
@@ -7,15 +7,23 @@
 //
 
 import AFNetworking
+import Parse
 
 class DCServiceCaller: NSObject {
 
 typealias Completion = (result: AnyObject?, error: NSError?) -> Void
 
     class func sendPush(pushText: String, completion: Completion) {
-        let params = ["where": ["deviceType": ["$in": ["ios"]]] ,"data": ["title": "Hey", "alert": "Hello"]]
-        let headers = ["X-Parse-Application-Id": "myAppId", "X-Parse-Master-Key": "myMasterKey"]
-        makeAPICall("https://arcane-fortress-10529.herokuapp.com/parse/push", method: "POST", headers: headers, params: params, completion: completion)
+//        let params = ["where": ["deviceType": ["$in": ["ios"]]] ,"data": ["title": "Hey", "alert": "Hello"]]
+//        let headers = ["X-Parse-Application-Id": "myAppId", "X-Parse-Master-Key": "myMasterKey"]
+//        
+//        makeAPICall("https://arcane-fortress-10529.herokuapp.com/parse/push", method: "POST", headers: headers, params: params, completion: completion)
+        
+        let pushQuery = PFInstallation.query()
+        pushQuery?.whereKey("deviceType", equalTo: "ios")
+        PFPush.sendPushMessageToQueryInBackground(pushQuery!, withMessage: "Hello") { (success: Bool, error: NSError?) in
+            completion(result: nil, error: error)
+        }
     }
     
     private class func makeAPICall(url: String, method: String, headers: [String: String]?, params: [String: AnyObject]?, completion: Completion) {
