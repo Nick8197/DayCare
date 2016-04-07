fe5bafb022c102abd555e83164dbdfb9214363be
Rishabh Tayal <rtayal11@gmail.com>
2016-04-06 22:38:48 -0500

    added diary view controller on day care

---
+++ CareTaker/CareTaker/DCDiaryViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DCDiaryViewController.swift
+//  CareTaker
+//
+//  Created by Tayal, Rishabh on 4/6/16.
+//  Copyright © 2016 Tayal, Rishabh. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class DCDiaryViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var dataArray: [DCDiary] = []
+    var refreshControl = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        refreshControl.addTarget(self, action: #selector(loadQuery), forControlEvents: .ValueChanged)
+        self.tableView.addSubview(refreshControl)
+        
+        loadQuery()
+    }
+    
+    func loadQuery() {
+        self.refreshControl.beginRefreshing()
+        let query = DCDiary.query()
+        //        query?.whereKey("child", containedIn: )
+        query?.includeKey("child")
+        query?.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) in
+            self.refreshControl.endRefreshing()
+            if let objects = objects {
+                self.dataArray = objects as! [DCDiary]
+                self.tableView.reloadData()
+            }
+        })
+    }
+}
+
+extension DCDiaryViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        
+        let obj = dataArray[indexPath.row]
+        cell?.textLabel?.text = obj.child.name
+        
+        return cell!
+    }
+}
+
+extension DCDiaryViewController: UITableViewDelegate {
+    
+}
