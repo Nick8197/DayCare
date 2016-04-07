c681021ea960703863931c1d1f6ae5bbf6b7ebfd
Rishabh Tayal <rtayal11@gmail.com>
2016-04-07 10:41:16 -0500

    Login UI improvemnts

--- Parents/DayCareClient/LoginViewController.swift
+++ Parents/DayCareClient/LoginViewController.swift
@@ -16,14 +16,26 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedOnView(_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    func tappedOnView(sender: AnyObject) {
+        self.view.endEditing(true)
     }
     
     @IBAction func loginTapped(sender: AnyObject) {
+        self.view.endEditing(true)
         User.logInWithUsernameInBackground(usernameTextfield.text!, password: passwordTextfield.text!) { (user: PFUser?, error: NSError?) in
             if let _ = user {
                 let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
                 appDelegate.showTabView()
             }
         }
+    }
+    
+    @IBAction func forgotTapped(sender: AnyObject) {
+        print("forgot tapped")
     }
 }
